7e6ca3aab775313939aec7634d8970759eabbef3
christophchamp <christophchamp@a738a490-a010-0410-b321-a8a109c63ca0>
2008-12-05 06:55:00 +0000

    added new/better Raster3D code + support for nucleotides.

--- tlsmd/bin/tlsmdlib/tls_animate.py
+++ tlsmd/bin/tlsmdlib/tls_animate.py
@@ -3,6 +3,8 @@
 ## This code is part of the TLSMD distribution and governed by
 ## its license.  Please see the LICENSE file that should have been
 ## included as part of this package.
+##
+## NOTE: Some of the code in here is used for rendering images with Raster3D
 
 import copy
 import string
@@ -10,14 +12,16 @@ import math
 import itertools
 import numpy
 
-from mmLib import Structure, FileIO, Gaussian, AtomMath, TLS
+from mmLib import Structure, FileIO, Gaussian, AtomMath, TLS, Constants
 
 import conf
+import console
 
 TWO_PI = 2.0 * math.pi
 
 DISPLACE_ATOM_NAME_DICT = {
-    "CA": True, "P": True, "O5*": True, "C5*": True, "C4*": True, "C3*": True, "O3*": True
+    "CA": True, "P": True, "O5*": True, "C5*": True, "C4*": True, "C3*": True, "O3*": True,
+    "O5'": True, "C5'": True, "C4'": True, "C3'": True, "O3'": True
     }
 
 def iter_filter_atoms(atom_iter):
@@ -105,13 +109,15 @@ class TLSAnimate(object):
         cp_struct.sort()
         return cp_struct
                 
-    def construct_animation(self, filename):
+    def construct_animation(self, filename, raw_r3d_filename):
         """Save the animated structure to the given filename.
         """
         ##self.phase_assignment()
+        raw_r3d_file = open(raw_r3d_filename, "w")
         for phase in (0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5):
-            self.construct_frame(phase)
+            self.construct_frame(phase, raw_r3d_file)
         FileIO.SaveStructure(struct = self.struct, fil = filename)
+        raw_r3d_file.close()
 
     def next_model_id(self):
         """Return the next availible model_id in self.struct
@@ -146,7 +152,7 @@ class TLSAnimate(object):
         self.L3_chain.set_chain_id(self.next_chain_id())
         self.struct.add_chain(self.L3_chain, True)
         
-    def construct_frame(self, phase):
+    def construct_frame(self, phase, raw_r3d_file):
         """Create a new model in self.struct with the TLS displacements
         caused from the three screw dispacement axes displaced by the
         sin(phase).
@@ -160,24 +166,44 @@ class TLSAnimate(object):
 
         ## now displace the new model according to the tls group
         ## screw axes and given phase
-        for tls in self.cpartition.iter_tls_segments():
-            self.displace_model(model, tls, phase)
+        #for tls in self.cpartition.iter_tls_segments():
+        #    self.displace_model(model, tls, phase)
 
-    def displace_model(self, model, tls, phase):
+        this_seg = ""
+        which_ntls = 0
+        for tls in self.cpartition.iter_tls_segments():
+            if str(this_seg) != str(tls):
+                which_ntls += 1
+            self.displace_model(model, tls, phase, which_ntls, raw_r3d_file)
+            this_seg = str(tls)
+
+
+    def displace_model(self, model, tls, phase, which_ntls, raw_r3d_file):
         """Displace the given model by the tls. 
         """
         tls_info  = tls.model_tls_info
         cor       = tls_info["COR"]
 
-        for n, Lx_rmsd, Lx_vec, Lx_rho, Lx_pitch in [
-            (1, "L1_rmsd", "L1_eigen_vec", "L1_rho", "L1_pitch"),
-            (2, "L2_rmsd", "L2_eigen_vec", "L2_rho", "L2_pitch"),
-            (3, "L3_rmsd", "L3_eigen_vec", "L3_rho", "L3_pitch") ]:
+        ## figure out which libration eigenvalue is the largest and
+        ## use that value in the animation. Christoph Champ, 2008-08-15
+        L1_val = float(tls_info["L1_eigen_val"]) * Constants.RAD2DEG2
+        L2_val = float(tls_info["L2_eigen_val"]) * Constants.RAD2DEG2
+        L3_val = float(tls_info["L3_eigen_val"]) * Constants.RAD2DEG2
+        max_libration = 0.00
+        for val in L1_val, L2_val, L3_val:
+            if val >= max_libration:
+                max_libration = val
+
+        for n, Lx_rmsd, Lx_val, Lx_vec, Lx_rho, Lx_pitch in [
+            (1, "L1_rmsd", "L1_eigen_val", "L1_eigen_vec", "L1_rho", "L1_pitch"),
+            (2, "L2_rmsd", "L2_eigen_val", "L2_eigen_vec", "L2_rho", "L2_pitch"),
+            (3, "L3_rmsd", "L3_eigen_val", "L3_eigen_vec", "L3_rho", "L3_pitch") ]:
 
             Lrmsd  = tls_info[Lx_rmsd]
             Lvec   = tls_info[Lx_vec]
             Lrho   = tls_info[Lx_rho]
             Lpitch = tls_info[Lx_pitch]
+            Lval   = tls_info[Lx_val] * Constants.RAD2DEG2
 
             ## pre-calculations for screw displacement
             Lorigin = cor + Lrho
@@ -199,3 +225,16 @@ class TLSAnimate(object):
                     for atm in frag.iter_atoms():
                         d = numpy.dot(D, atm.position - Lorigin) + d_screw
                         atm.position += d
+
+                        atm.temp_factor = float(which_ntls)
+                        atm.element = str(model.model_id)
+
+                        ## raw input file for tlsanim2r3d->Raster3D
+                        if Lval == max_libration:
+                            raw_r3d_file.write("1 ")
+                        else:
+                            raw_r3d_file.write("0 ")
+
+                        raw_r3d_file.write("%s %s %s %s %.3f %.3f %.3f\n" % (
+                            model.model_id, self.L1_chain.chain_id, which_ntls, n,
+                            atm.position[0], atm.position[1], atm.position[2]))
