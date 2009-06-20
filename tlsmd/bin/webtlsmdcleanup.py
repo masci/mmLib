6d0b0058271dde41717a9e356325b5d1376f5754
christophchamp <christophchamp@a738a490-a010-0410-b321-a8a109c63ca0>
2009-06-20 04:05:55 +0000

    switched to load job_list from MySQL DB.

--- tlsmd/bin/webtlsmdcleanup.py
+++ tlsmd/bin/webtlsmdcleanup.py
@@ -5,66 +5,66 @@
 ## its license.  Please see the LICENSE file that should have been
 ## included as part of this package.
 
+## Pyton modules
 import os
 import sys
 import time
-import datetime # Christoph Champ, 2008-01-29
 import socket
-
 import xmlrpclib
 
-from tlsmdlib import const, conf
+## TLSMD
+from tlsmdlib import const, conf, misc, mysql_support
 
 SECS_IN_DAY = float(60*60*24)
 DELETE_DAYS = 14
 
 ## GLOBALS
 webtlsmdd = xmlrpclib.ServerProxy(conf.WEBTLSMDD)
+mysql = mysql_support.MySQLConnect()
 
 def check_remove(jdict):
     state = jdict.get("state")
-    if state==None:
+    if state == None:
 	return True
 
     submit_time = jdict.get("submit_time")
-    if submit_time==None:
+    if submit_time == None:
         return False
 
     email = jdict.get("email")
     if email: pass
 
-    days = round((time.time() - submit_time) / SECS_IN_DAY)
+    days = round((time.time() - float(submit_time)) / SECS_IN_DAY)
     jdict["days"] = days
 
-    if days>DELETE_DAYS:
+    if days > DELETE_DAYS:
         return True
 
-    if state=="submit1" and days>1:
+    if state == "submit1" and days > 1:
         return True
 
-    if state=="lost_directory" and days>1:
+    if state == "lost_directory" and days > 1:
 	return True
 
     return False
 
 def main():
-    ## timstamp format: "YYYY-MM-DD HH:MM:SS:"
-    t = datetime.datetime.fromtimestamp(time.time()).isoformat(' ')[:-7]
-
     print "[%s] WebTLSMD Job Cleanup: Checking database for old jobs to remove." % t
 
-    job_list = webtlsmdd.job_list()
+    job_list = mysql.job_list()
     jdict_remove_list = []
 
     for jdict in job_list:
         if check_remove(jdict):
             jdict_remove_list.append(jdict)
 
+    t = misc.timestamp()
     for jdict in jdict_remove_list:
 	try:
             print "[%s] %10s  %40s  %d Days Old" % (t, jdict["job_id"], jdict.get("email", "No Email"), jdict["days"])
 	except:
             print "[%s] %10s  Bad submission status" % (t, jdict["job_id"])
+        mysql.archive_old_jobs(jdict["job_id"])
         webtlsmdd.remove_job(jdict["job_id"])
 
 
