0f175c9134ca87e88df0dd2226f5198c1b0d499a
christophchamp <christophchamp@a738a490-a010-0410-b321-a8a109c63ca0>
2009-09-22 00:05:46 +0000

    either dump all jobIDs in database or only user selected.

--- tlsmd/bin/inspect_jobid.py
+++ tlsmd/bin/inspect_jobid.py
@@ -8,6 +8,7 @@ import xmlrpclib
 from tlsmdlib import const, conf
 
 webtlsmdd = xmlrpclib.ServerProxy(conf.WEBTLSMDD)
+DUMP_ALL = True
 
 def main():
     if len(sys.argv) != 2:
@@ -15,10 +16,15 @@ def main():
         sys.exit()
 
     job_list = webtlsmdd.job_list()
-    for jdict in job_list:
-        if sys.argv[1] in jdict.values():
+    if DUMP_ALL:
+        for jdict in job_list:
             for k,v in jdict.iteritems():
                 print k, ":", v
+    elif DUMP_ALL == False:
+        for jdict in job_list:
+            if sys.argv[1].strip("/") in jdict.values():
+                for k,v in jdict.iteritems():
+                    print k, ":", v
 
 if __name__ == "__main__":
     main()
